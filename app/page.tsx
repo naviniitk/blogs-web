9712450395298754c6eadf5b9a8d2a7f893eff75
Naveen Kumar <naveen.umar@jalantechnologies.com>
2023-08-05 21:30:58 +0530

    added article about history of astronomy

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import Link from "next/link"
 
 export default function Home() {
   return (
-    <div className="prose dark:prose-invert">
+    <div className="prose dark:prose-invert max-w-4xl justify-center mx-auto">
       {allPosts.map((post) => (
-        <article key={post._id}>
+        <article className="p-4 rounded-xl my-4 border" key={post._id}>
           <Link href={post.slug}>
             <h2>{post.title}</h2>
           </Link>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,22 @@ module.exports = {
   ],
   darkMode: ["class"],
   theme: {
+    colors: {
+      "timber": "#E7DCDA",
+      "sky-dark": {
+        100: "#A7CDBD",
+        200: "#97C4B1",
+        300: "#7CB69E",
+        400: "#62A78A",
+        500: "#519075",
+        600: "#42765F",
+        700: "#335B49",
+        800: "#254134",
+        900: "#16271F",
+        950: "#161413",
+      },
+    },
     extend: {},
   },
   plugins: [require("@tailwindcss/typography")],
-}
+};
